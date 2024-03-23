dc1e6f1a1c7fdafc72458f76efe3485b0e5440b7
Eren Elçi <elcierenn@gmail.com>
2024-03-23 05:09:12 +0300

    Food Delivery App Architecture part2b2 kütüphaneler kuruldu Stacked service için gereklikler baslandi

---
+++ lib/app/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_delivery_app/ui/detail/detail_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: SplahView, initial: true),
+  ],
+)
+class App {}

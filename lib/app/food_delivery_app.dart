9a1f5e5bfe065a4837c8e68b4a4d7572c2db9809
Eren Elçi <elcierenn@gmail.com>
2024-03-23 04:37:41 +0300

    Food Delivery App Architecture part2b1

--- lib/app/food_delivery_app.dart
+++ lib/app/food_delivery_app.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_delivery_app/ui/main/main_view.dart';
 
 class FoodDeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+      DeviceOrientation
+          .portraitUp //app başladığı itibaren portrait olarak al yani sadece dik dursun
+    ]);
     return const MaterialApp(
       title: "Food Delivery App",
       debugShowCheckedModeBanner: false,
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_delivery_app/app/food_delivery_app.dart';
 
 void main() {
-  WidgetsFlutterBinding
-      .ensureInitialized; //Bu sayede, uygulama çalıştırılmaya hazır olduğunda herhangi bir beklenmedik durum oluşmasını önler.
+  WidgetsFlutterBinding.ensureInitialized;
   runApp(FoodDeliveryApp());
 }

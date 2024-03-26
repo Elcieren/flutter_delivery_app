e467b092f880fa10c2554027288a549e1b61c219
Eren Elçi <elcierenn@gmail.com>
2024-03-26 04:46:41 +0300

    Food Delivery App App UI Part 2

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -14,57 +14,82 @@ class HomeView extends StatelessWidget {
         disposeViewModel: false,
         builder: (context, viewModel, child) => viewModel.categories == null
             ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: viewModel.categories!.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0)),
-                    child: Container(
-                      width: 300,
-                      child: Column(
+            : SafeArea(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
                         children: [
-                          Container(
-                            height: 220,
-                            width: 300,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.fitWidth,
-                                image: NetworkImage(viewModel
-                                    .categories![index].strCategoryThumb!),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              viewModel.categories![index].strCategory!,
-                              maxLines: 2,
-                              softWrap: true,
-                              textAlign: TextAlign.start,
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 8.0),
-                            child: Text(
-                              viewModel
-                                  .categories![index].strCategoryDescription!,
-                              maxLines: 3,
-                              softWrap: true,
-                              textAlign: TextAlign.start,
-                              style: TextStyle(fontSize: 16),
-                            ),
+                          Text(
+                            "Categorieies",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 30),
                           ),
                         ],
                       ),
                     ),
-                  );
-                }) /* Center(
-            child: Text(viewModel.categories != null
-                ? viewModel.categories![1].strCategory
-                : "Loading")) */
-        );
+                    Container(
+                      height: 350,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: viewModel.categories!.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              elevation: 4,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    width: 300,
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          height: 220,
+                                          width: 300,
+                                          decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                              fit: BoxFit.fitWidth,
+                                              image: NetworkImage(viewModel
+                                                  .categories![index]
+                                                  .strCategoryThumb!),
+                                            ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            viewModel.categories![index]
+                                                .strCategory!,
+                                            maxLines: 2,
+                                            softWrap: true,
+                                            textAlign: TextAlign.start,
+                                            style: TextStyle(fontSize: 16),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              bottom: 8.0),
+                                          child: Text(
+                                            viewModel.categories![index]
+                                                .strCategoryDescription!,
+                                            maxLines: 3,
+                                            softWrap: true,
+                                            textAlign: TextAlign.start,
+                                            style: TextStyle(fontSize: 16),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+              ));
   }
 }
